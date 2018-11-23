a4b1f7310dbbf5bec78517d45726824297b76ee8
kdrag0n <dragon@khronodragon.com>
2018-11-22 18:01:56 -0800

    maple-iosched: convert to state notifier
    Signed-off-by: kdrag0n <dragon@khronodragon.com>

--- block/maple-iosched.c
+++ block/maple-iosched.c
@@ -17,7 +17,7 @@
 #include <linux/module.h>
 #include <linux/init.h>
 #include <linux/slab.h>
-#include <linux/fb.h>
+#include <linux/state_notifier.h>
 
 #define MAPLE_IOSCHED_PATCHLEVEL	(8)
 
@@ -90,10 +90,10 @@ maple_add_request(struct request_queue *q, struct request *rq)
 
    	/* inrease expiration when device is asleep */
    	unsigned int fifo_expire_suspended = mdata->fifo_expire[sync][dir] * sleep_latency_multiple;
-   	if (mdata->display_on && mdata->fifo_expire[sync][dir]) {
+   	if (!state_suspended && mdata->fifo_expire[sync][dir]) {
         rq->fifo_time = jiffies + mdata->fifo_expire[sync][dir];
    		list_add_tail(&rq->queuelist, &mdata->fifo_list[sync][dir]);
-   	} else if (!mdata->display_on && fifo_expire_suspended) {
+   	} else if (state_suspended && fifo_expire_suspended) {
         rq->fifo_time = jiffies + fifo_expire_suspended;
    		list_add_tail(&rq->queuelist, &mdata->fifo_list[sync][dir]);
    	}
@@ -220,9 +220,9 @@ maple_dispatch_requests(struct request_queue *q, int force)
 	/* Retrieve request */
 	if (!rq) {
 		/* Treat writes fairly while suspended, otherwise allow them to be starved */
-		if (mdata->display_on && mdata->starved >= mdata->writes_starved)
+		if (!state_suspended && mdata->starved >= mdata->writes_starved)
 			data_dir = WRITE;
-		else if (!mdata->display_on && mdata->starved >= 1)
+		else if (state_suspended && mdata->starved >= 1)
 			data_dir = WRITE;
 
 		rq = maple_choose_request(mdata, data_dir);
