cff831b8c5700f208d16b0ceef0347da5ff70d8a
alfael <alfael.sc@gmail.com>
2020-12-01 14:48:49 +0100

    ALFAK2.7 update build.sh

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-#export CROSS_COMPILE=/home/schailan/Documents/kernel/toolchain/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-#export PATH=$PATH:/home/schailan/Documents/kernel/toolchain/arm-linux-androideabi-4.9/bin/:/home/schailan/Documents/kernel/toolchain/gcc-linaro-4.9.4-2017.01-x86_64_aarch64-elf/bin/
+cd $(dirname $0)
 export CROSS_COMPILE=/home/alfael/toolchain/gcc-linaro-7.5.0-2019.12-x86_64_aarch64-elf/bin/aarch64-elf-
 export CROSS_COMPILE_ARM32=/home/alfael/toolchain/gcc-linaro-7.5.0-2019.12-x86_64_arm-eabi/bin/arm-eabi-
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_CFLAGS="-Wno-maybe-uninitialized -Wno-memset-elt-size -Wno-duplicate-decl-specifier"
+if [ ! -d "./output/" ]; then
+        mkdir ./output/
+fi
 rm -r ./output/*
 make O=output clean
 make O=output mrproper
