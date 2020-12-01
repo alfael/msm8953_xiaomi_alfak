2fd786ab081aef92d8baacc453fd37584297a4a6
alfael <alfael.sc@gmail.com>
2020-12-01 14:25:45 +0100

    TWEAK CFS

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -82,8 +82,8 @@ walt_dec_cfs_rq_stats(struct cfs_rq *cfs_rq, struct task_struct *p) {}
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-unsigned int sysctl_sched_latency = 15000000ULL;
-unsigned int normalized_sysctl_sched_latency = 15000000ULL;
+unsigned int sysctl_sched_latency = 3000000ULL;
+unsigned int normalized_sysctl_sched_latency = 3000000ULL;
 
 unsigned int sysctl_sched_is_big_little = 1;
 unsigned int sysctl_sched_sync_hint_enable = 1;
@@ -110,8 +110,8 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling
  * Minimal preemption granularity for CPU-bound tasks:
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity = 1000000ULL;
-unsigned int normalized_sysctl_sched_min_granularity = 1000000ULL;
+unsigned int sysctl_sched_min_granularity = 600000ULL;
+unsigned int normalized_sysctl_sched_min_granularity = 600000ULL;
 
 /*
  * is kept at sysctl_sched_latency / sysctl_sched_min_granularity
@@ -132,10 +132,10 @@ unsigned int sysctl_sched_child_runs_first __read_mostly = 1U;
  * and reduces their over-scheduling. Synchronous workloads will still
  * have immediate wakeup/sleep latencies.
  */
-unsigned int sysctl_sched_wakeup_granularity = 2000000ULL;
-unsigned int normalized_sysctl_sched_wakeup_granularity = 2000000ULL;
+unsigned int sysctl_sched_wakeup_granularity = 500000ULL;
+unsigned int normalized_sysctl_sched_wakeup_granularity = 500000ULL;
 
-const_debug unsigned int sysctl_sched_migration_cost = 400000ULL;
+const_debug unsigned int sysctl_sched_migration_cost = 300000ULL;
 
 /*
  * The exponential sliding  window over which load is averaged for shares
@@ -155,7 +155,7 @@ unsigned int __read_mostly sysctl_sched_shares_window = 10000000ULL;
  *
  * default: 5 msec, units: microseconds
   */
-unsigned int sysctl_sched_cfs_bandwidth_slice = 5000UL;
+unsigned int sysctl_sched_cfs_bandwidth_slice = 4000UL;
 #endif
 
 /*
