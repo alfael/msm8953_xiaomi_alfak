0ef419bab1ef0e4d0fca1266d68bce60cdeb6afb
alfael <alfael.sc@gmail.com>
2021-02-22 14:49:37 +0100

    build.sh: Improve build script

--- build_clang.sh
+++ build_clang.sh
@@ -9,43 +9,56 @@ export KBUILD_CFLAGS="-Wno-maybe-uninitialized -Wno-memset-elt-size -Wno-duplica
 if [ ! -d "./output/" ]; then
         mkdir ./output/
 fi
-rm -r ./output/*
+#rm -r ./output/*
 make CC=clang O=output clean
 make CC=clang O=output mrproper
 make CC=clang O=output CC=clang tissot_alfak_defconfig
 #make O=output menuconfig
 make CC=clang O=output CC=clang AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip  -j$(nproc --all) 2>&1 | tee build.log
 
-PATH_OUTPUT=/home/alfael/kernel/msm8953_xiaomi_alfak/output/arch/arm64/boot
+PATH_OUTPUT=output/arch/arm64/boot
 PATH_KERN=$PATH_OUTPUT/Image.gz
 PATH_QCOM=$PATH_OUTPUT/dts/qcom/
-PATH_PACKAGE=/home/alfael/kernel/msm8953_xiaomi_alfak/package
-PATH_OUTPUT_PACKAGE=/home/alfael/Shared/final_package
+PATH_PACKAGE=package
+PATH_OUTPUT_PACKAGE=~/Shared/final_package
+
+echo $PATH_OUTPUT
+echo $PATH_KERN
+echo $PATH_QCOM
+echo $PATH_PACKAGE
+echo $PATH_OUTPUT_PACKAGE
+
 if [ ! -f "$PATH_KERN" ]; then
         echo Erreur de compilation avortement...
         exit;
 fi
 echo Compilation terminée !
 echo Création du package flashable...
-
 while read line
 do
-        if grep -q "EXTRAVERSION = " <<< "$line"; then
-                EXTRAVERSION=$(cut -d "-" -f2 <<< "$line" | xargs)
-                continue;
-        fi
         if grep -q "SUBLEVEL = " <<< "$line"; then
                 SUBLEVEL=$(cut -d "=" -f2 <<< "$line" | xargs)
                 continue;
         fi
-        if [ ! -z "$EXTRAVERSION" ] || [  ! -z "$SUBLEVEL" ]; then
+        if [ ! -z "$SUBLEVEL" ]; then
                 break;
         fi
 done < Makefile
+while read line
+do
+        if grep -q "LOCALVERSION=" <<< "$line"; then
+                LOCALVERSION=$(cut -d "-" -f2 <<< "$line" | xargs -0 | tr -d '"')
+                continue;
+        fi
+        if [ ! -z "$LOCALVERSION" ]; then
+                break;
+        fi
+done < arch/arm64/configs/tissot_alfak_defconfig
+
 /bin/cp -rf $PATH_KERN $PATH_PACKAGE/kernel
 /bin/cp -rf $PATH_QCOM/msm8953-qrd-sku3-tissot-treble.dtb $PATH_PACKAGE/dtb-treble
 /bin/cp -rf $PATH_QCOM/msm8953-qrd-sku3-tissot-nontreble.dtb $PATH_PACKAGE/dtb-nontreble
 cd $PATH_PACKAGE
-zip -0 -r $PATH_OUTPUT_PACKAGE/$EXTRAVERSION.$SUBLEVEL.zip ./*
+zip -0 -r $PATH_OUTPUT_PACKAGE/$LOCALVERSION.$SUBLEVEL.zip ./*
 cd -;
-echo Création du package: $EXTRAVERSION.$SUBLEVEL.zip terminée !
+echo Création du package: $LOCALVERSION.$SUBLEVEL.zip terminée !
